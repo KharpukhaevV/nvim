2ef759f86a5fec94795e110988c0127f67ff345c
admin <admin@m.com>
2025-07-29 01:45:50 +0400

    rerange tabs

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,10 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+-- Установка табуляции в 4 пробела
+vim.opt.tabstop = 4       -- Отображение табуляции как 4 пробела
+vim.opt.shiftwidth = 4    -- Автоматический отступ = 4 пробела
+vim.opt.softtabstop = 4   -- Количество пробелов при нажатии Tab
+-- vim.opt.expandtab = true  -- Заменять табы на пробелы
