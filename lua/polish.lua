55df4be815260a2e5bab281d4a404788f563867b
admin <admin@m.com>
2025-08-13 02:09:07 +0400

    update navigate in insert mode

--- lua/polish.lua
+++ lua/polish.lua
@@ -7,4 +7,12 @@
 vim.opt.tabstop = 4       -- Отображение табуляции как 4 пробела
 vim.opt.shiftwidth = 4    -- Автоматический отступ = 4 пробела
 vim.opt.softtabstop = 4   -- Количество пробелов при нажатии Tab
+-- Настройка навигации в режиме вставки
+vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true })
+
+vim.api.nvim_set_keymap('i', '<C-e>', '<C-o>e', { noremap = true })  -- Переместить на конец слова
+vim.api.nvim_set_keymap('i', '<C-b>', '<C-o>b', { noremap = true }) 
 -- vim.opt.expandtab = true  -- Заменять табы на пробелы
