ca3fd35d9a6be6fb116a8acd8820c7c3ebee1c11
admin <admin@m.com>
2025-09-04 21:12:36 +0400

    add new commands in insert mode

--- lua/polish.lua
+++ lua/polish.lua
@@ -8,12 +8,32 @@ vim.opt.tabstop = 4       -- Отображение табуляции как 4 
 vim.opt.shiftwidth = 4    -- Автоматический отступ = 4 пробела
 vim.opt.softtabstop = 4   -- Количество пробелов при нажатии Tab
 -- Настройка навигации в режиме вставки
-vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<M-h>', '<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<M-l>', '<Right>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<M-j>', '<Down>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<M-k>', '<Up>', { noremap = true })
+-- Перемещение по словам в режиме вставки
+vim.api.nvim_set_keymap('i', '<C-M-h>', '<C-Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-M-l>', '<C-Right>', { noremap = true })
+
+-- Удаление слова в режиме вставки
+vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', { noremap = true })
+
 
 vim.api.nvim_set_keymap("n", "<C-M-l>", ":lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
+
+
+-- Определение функции для отмены в режиме вставки
+vim.api.nvim_exec([[
+function! UndoInInsertMode()
+    execute "normal! u"
+    startinsert
+endfunction
+]], false)
+
+-- Настройка комбинации клавиш для отмены в режиме вставки
+vim.api.nvim_set_keymap('i', '<C-z>', '<C-o>:call UndoInInsertMode()<CR>', { noremap = true })
+
 -- vim.api.nvim_set_keymap('i', '<C-e>', '<C-o>e', { noremap = true })  -- Переместить на конец слова
 -- vim.api.nvim_set_keymap('i', '<C-b>', '<C-o>b', { noremap = true }) 
 -- vim.opt.expandtab = true  -- Заменять табы на пробелы
